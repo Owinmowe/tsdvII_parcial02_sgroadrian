a9c637cd0255c05d471e253b6524bfdf17e9d662
Adrian <Owinmowe@gmail.com>
2021-06-24 02:22:04 -0300

    Added basic menu animations with corresponding prefabs.

---
+++ Sgro Adrian - Lunar Lander - Parcial 2/Assets/Scripts/UI/UI_Component.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class UI_Component : MonoBehaviour
+{
+
+    public Action OnTransitionEnd;
+
+    Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void TransitionEnd()
+    {
+       OnTransitionEnd?.Invoke();
+    }
+
+    public void TransitionIn()
+    {
+        anim.SetTrigger("Transition In");
+    }
+
+    public void TransitionOut()
+    {
+        anim.SetTrigger("Transition Out");
+    }
+}
---
+++ Sgro Adrian - Lunar Lander - Parcial 2/Assets/Scripts/UI/UI_MainMenu.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_MainMenu : MonoBehaviour
+{
+
+    [SerializeField] List<UI_Component> splashArts = null;
+    [Space(10)]
+    [SerializeField] UI_Component generalBackground;
+    [SerializeField] List<UI_Component> mainMenuComponents = null;
+    [SerializeField] List<UI_Component> creditsMenuComponents = null;
+
+    int currentSplashArt = 0;
+
+    private void Start()
+    {
+        foreach (var item in splashArts)
+        {
+            item.OnTransitionEnd += NextSplashArt;
+        }
+        if(splashArts != null)
+        {
+            splashArts[0].TransitionIn();
+        }
+    }
+
+    void NextSplashArt()
+    {
+        currentSplashArt++;
+        if (currentSplashArt < splashArts.Count)
+        {
+            splashArts[currentSplashArt].TransitionIn();
+        }
+        else
+        {
+            generalBackground.TransitionIn();
+            StartMenu();
+        }
+    }
+
+    void StartMenu()
+    {
+        foreach (var item in creditsMenuComponents)
+        {
+            item.TransitionOut();
+        }
+        foreach (var item in mainMenuComponents)
+        {
+            item.TransitionIn();
+        }
+    }
+
+    public void StartCredits()
+    {
+        foreach (var item in mainMenuComponents)
+        {
+            item.TransitionOut();
+        }
+        foreach (var item in creditsMenuComponents)
+        {
+            item.TransitionIn();
+        }
+    }
+}
