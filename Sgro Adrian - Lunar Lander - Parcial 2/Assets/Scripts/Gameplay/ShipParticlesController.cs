34ecf56db056f2ffcec7932582dcd151e022fb0f
Adrian <Owinmowe@gmail.com>
2021-06-24 02:25:02 -0300

    Added basic particles controller with connection to the ship class.

---
+++ Sgro Adrian - Lunar Lander - Parcial 2/Assets/Scripts/Gameplay/ShipParticlesController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ShipMovement))]
+public class ShipParticlesController : MonoBehaviour
+{
+    [Header("Particle System")]
+    [SerializeField] ParticleSystem thrustParticles = null;
+    [SerializeField] int minParticlesThrust = 0;
+    [SerializeField] int maxParticlesThrust = 100;
+    [SerializeField] int particlesAccelerationSpeed = 5;
+
+    ParticleSystem.EmissionModule emissionModule;
+
+    bool accelerating = false;
+
+    private void Awake()
+    {
+        emissionModule = thrustParticles.emission;
+        GetComponent<ShipMovement>().OnAcceleration += IncrementParticles;
+    }
+
+    void IncrementParticles()
+    {
+        accelerating = true;
+        if (emissionModule.rateOverTime.constant < maxParticlesThrust + particlesAccelerationSpeed)
+        {
+            emissionModule.rateOverTime = emissionModule.rateOverTime.constant + particlesAccelerationSpeed;
+        }
+        else
+        {
+            emissionModule.rateOverTime = particlesAccelerationSpeed;
+        }
+    }
+
+    private void Update()
+    {
+        if (accelerating)
+        {
+            accelerating = false;
+            return;
+        }
+        if (emissionModule.rateOverTime.constant < minParticlesThrust + particlesAccelerationSpeed)
+        {
+            emissionModule.rateOverTime = minParticlesThrust;
+        }
+        else
+        {
+            emissionModule.rateOverTime = emissionModule.rateOverTime.constant - particlesAccelerationSpeed;
+        }
+    }
+
+}
