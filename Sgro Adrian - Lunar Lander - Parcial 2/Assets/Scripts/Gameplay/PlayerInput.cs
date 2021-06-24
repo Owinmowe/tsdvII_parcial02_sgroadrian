857192ad7ad74697651151c55adc249e31381c8e
Adrian <Owinmowe@gmail.com>
2021-06-24 02:24:33 -0300

    Added player input script with connection to the ship movement class.

---
+++ Sgro Adrian - Lunar Lander - Parcial 2/Assets/Scripts/Gameplay/PlayerInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ShipMovement))]
+public class PlayerInput : MonoBehaviour
+{
+
+    ShipMovement shipMovement = null;
+
+    private void Awake()
+    {
+        shipMovement = GetComponent<ShipMovement>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Space))
+        {
+            shipMovement.Move(MovementType.Accelerate);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            shipMovement.Move(MovementType.TurnLeft);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            shipMovement.Move(MovementType.TurnRight);
+        }
+    }
+}
