1cf87ffb4fb8cabe7dbc75a280c6285a596ad755
Adrian <Owinmowe@gmail.com>
2021-06-27 00:55:03 -0300

    Added removed data from ship class as a ship configuration SO.

---
+++ Sgro Adrian - Lunar Lander - Parcial 2/Assets/Scripts/ScriptableObjects/ShipConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Ship Data", menuName = "Ship/Data")]
+public class ShipConfiguration : ScriptableObject
+{
+    [Header("Movement")]
+    public float accelerateSpeed = .2f;
+    public float turnSpeed = 1f;
+    [Range(0, 360)] public float turnRange = 180;
+    public float mass = 1f;
+    public float linearDrag = .1f;
+    public float gravityScale = .01f;
+
+    [Header("Collision")]
+    public CapsuleDirection2D direction = CapsuleDirection2D.Vertical;
+    public Vector2 colliderSize = new Vector2(.4f, .6f);
+
+    [Header("Landing")]
+    public float landingAngleTolerance = 10f;
+    public float landingSpeedTolerance = 10f;
+    public LayerMask landingLayer = default;
+    public float checkDistance = .5f;
+}
