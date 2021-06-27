a445386c08ddf3ace790ed2dc10d69867bdb454b
Adrian <Owinmowe@gmail.com>
2021-06-27 00:49:18 -0300

    Added ship animation controller as a separate component.

---
+++ Sgro Adrian - Lunar Lander - Parcial 2/Assets/Scripts/Gameplay/ShipAnimationController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipAnimationController : MonoBehaviour
+{
+    [SerializeField] Ship ship = null;
+    [SerializeField] string shipExplosionTrigger = "Explosion";
+    [SerializeField] string shipSuccessTrigger = "Landed";
+
+    Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        if(ship != null)
+        {
+            ship.OnLanding += ShipLandAnimation;
+        }   
+    }
+
+    void ShipLandAnimation(bool collisionSuccess)
+    {
+        if (collisionSuccess) anim.SetTrigger(shipSuccessTrigger);
+        else anim.SetTrigger(shipExplosionTrigger);
+    }
+
+}
