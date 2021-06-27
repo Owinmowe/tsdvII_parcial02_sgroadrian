63f984f6ba8b929008d546501c377157ccb1c09e
Adrian <Owinmowe@gmail.com>
2021-06-27 01:04:54 -0300

    Created basic loader script with corresponding UI component.

---
+++ Sgro Adrian - Lunar Lander - Parcial 2/Assets/Scripts/LoaderManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoaderManager : MonoBehaviourSingleton<LoaderManager>
+{
+
+    [SerializeField] float minTimeToLoadScene = 1f;
+    [SerializeField] UI_LoadingScreen uI_LoadingScreen = null;
+
+    public void LoadSceneAsync(string sceneName)
+    {
+        StartCoroutine(AsynchronousLoadWithFake(sceneName));
+    }
+
+    IEnumerator AsynchronousLoadWithFake(string scene)
+    {
+        float loadingProgress = 0;
+        float timeLoading = 0;
+
+        yield return null;
+
+        AsyncOperation ao = SceneManager.LoadSceneAsync(scene);
+        uI_LoadingScreen.FadeWithBlackScreen();
+        uI_LoadingScreen.LockFade();
+        ao.allowSceneActivation = false;
+
+        while (!ao.isDone)
+        {
+            timeLoading += Time.deltaTime;
+            loadingProgress = ao.progress + 0.1f;
+            loadingProgress = loadingProgress * timeLoading / minTimeToLoadScene;
+
+            // Se completo la carga
+            if (loadingProgress >= 1)
+            {
+                ao.allowSceneActivation = true;
+                uI_LoadingScreen.UnlockFade();
+            }
+            yield return null;
+        }
+
+    }
+}
---
+++ Sgro Adrian - Lunar Lander - Parcial 2/Assets/UI_LoadingScreen.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_LoadingScreen : MonoBehaviour
+{
+    [SerializeField] Image blackScreen;
+
+    bool canFadeOut = true;
+
+    public void FadeWithBlackScreen()
+    {
+        StopAllCoroutines();
+        StartCoroutine(BlackScreenFade());
+    }
+
+    IEnumerator BlackScreenFade()
+    {
+        while(blackScreen.color.a + Time.deltaTime < 1)
+        {
+            blackScreen.color = new Color(blackScreen.color.r, blackScreen.color.g, blackScreen.color.b, blackScreen.color.a + Time.deltaTime);
+            yield return null;
+        }
+        blackScreen.color = Color.black;
+        while (!canFadeOut)
+        {
+            yield return null;
+        }
+        while (blackScreen.color.a - Time.deltaTime > 0)
+        {
+            blackScreen.color = new Color(blackScreen.color.r, blackScreen.color.g, blackScreen.color.b, blackScreen.color.a - Time.deltaTime);
+            yield return null;
+        }
+        blackScreen.color = new Color(blackScreen.color.r, blackScreen.color.g, blackScreen.color.b, 0);
+    }
+
+    public void LockFade()
+    {
+        canFadeOut = false;
+    }
+
+    public void UnlockFade()
+    {
+        canFadeOut = true;
+    }
+}
