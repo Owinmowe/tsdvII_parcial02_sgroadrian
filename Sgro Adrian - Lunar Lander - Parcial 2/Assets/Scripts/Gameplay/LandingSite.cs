5d6c29e34511101a6565cb3669d58e0b1fe60639
Adrian <Owinmowe@gmail.com>
2021-06-27 01:03:44 -0300

    Added landing site prefab with corresponding creation on terrain.

---
+++ Sgro Adrian - Lunar Lander - Parcial 2/Assets/Scripts/Gameplay/LandingSite.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class LandingSite : MonoBehaviour
+{
+
+    int points = 0;
+    [SerializeField] GameObject LandingLine = null;
+    [SerializeField] TextMeshPro textComponent = null;
+    [SerializeField] float textBlinkSpeed = 1f;
+
+    private void Awake()
+    {
+        GetComponent<Animator>().SetFloat("BlinkSpeed", textBlinkSpeed);
+    }
+
+    public void SetLandingType(int points, float colliderXSize, Color color)
+    {
+        this.points = points;
+        gameObject.name = points.ToString() + "X";
+        textComponent.text = points.ToString() + "X";
+        textComponent.color = color;
+        LandingLine.GetComponent<SpriteRenderer>().color = color;
+        LandingLine.transform.localScale = new Vector2(colliderXSize, LandingLine.transform.localScale.y); 
+    }
+}
