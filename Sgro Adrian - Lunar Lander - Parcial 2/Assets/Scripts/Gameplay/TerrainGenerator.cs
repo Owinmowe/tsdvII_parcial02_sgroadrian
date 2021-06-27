7b109ec72852163a08fabc9b032a4cee4d2fd8a8
Adrian <Owinmowe@gmail.com>
2021-06-27 00:46:22 -0300

    Improved terrain data from script by using Scriptable objects.

---
+++ Sgro Adrian - Lunar Lander - Parcial 2/Assets/Scripts/Gameplay/TerrainGenerator.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class TerrainGenerator : MonoBehaviour
+{
+
+    [SerializeField] TerrainConfiguration terrainConfig = null;
+
+    Spline terrainSpline;
+    List<int> usedIndex = new List<int>();
+
+    void Start()
+    {
+        GameObject go = Instantiate(terrainConfig.terrainBase);
+        go.transform.parent = transform;
+        terrainSpline = go.GetComponent<SpriteShapeController>().spline;
+        SetTerrainSize();
+        SetNewRandomPoints();
+        SmoothTerrain();
+        SetLandingSites();
+    }
+
+    void SetTerrainSize()
+    {
+        terrainSpline.SetPosition(0, terrainSpline.GetPosition(0) - Vector3.right * terrainConfig.terrainSize / 2);
+        terrainSpline.SetPosition(1, terrainSpline.GetPosition(1) - Vector3.right * terrainConfig.terrainSize / 2);
+        terrainSpline.SetPosition(2, terrainSpline.GetPosition(2) + Vector3.right * terrainConfig.terrainSize / 2);
+        terrainSpline.SetPosition(3, terrainSpline.GetPosition(3) + Vector3.right * terrainConfig.terrainSize / 2);
+    }
+
+    void SetNewRandomPoints()
+    {
+        float distanceBetweenPoints = (float)terrainConfig.terrainSize / terrainConfig.terrainPoints;
+        for (int i = 0; i < terrainConfig.terrainPoints; i++)
+        {
+            float posX = terrainSpline.GetPosition(i + 1).x + distanceBetweenPoints;
+            Vector3 RandomPos = new Vector3(posX, terrainConfig.perlinMultiplier * Mathf.PerlinNoise(Random.Range(terrainSpline.GetPosition(i + 2).x - terrainConfig.nextPointRange, terrainSpline.GetPosition(i + 2).x + terrainConfig.nextPointRange), i + 2));
+            terrainSpline.InsertPointAt(i + 2, RandomPos);
+        }
+    }
+
+    void SmoothTerrain()
+    {
+        for (int i = 2; i < terrainConfig.terrainPoints + 2; i++)
+        {
+            switch (terrainConfig.currentTerrainLinesTypes)
+            {
+                case TerrainLinesTypes.Linear:
+                    terrainSpline.SetTangentMode(i, ShapeTangentMode.Linear);
+                    break;
+                case TerrainLinesTypes.Curved:
+                    terrainSpline.SetTangentMode(i, ShapeTangentMode.Continuous);
+                    terrainSpline.SetLeftTangent(i, Vector3.left * 0.5f);
+                    terrainSpline.SetRightTangent(i, Vector3.right * 0.5f);
+                    break;
+                case TerrainLinesTypes.Both:
+                    int aux = Random.Range(0, 1);
+                    ShapeTangentMode nextShape = aux == 0 ? ShapeTangentMode.Continuous : ShapeTangentMode.Linear;
+                    terrainSpline.SetTangentMode(i, nextShape);
+                    terrainSpline.SetLeftTangent(i, Vector3.left * 0.5f);
+                    terrainSpline.SetRightTangent(i, Vector3.right * 0.5f);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    void SetLandingSites()
+    {
+        if(terrainConfig.landings2XPerTerrain + terrainConfig.landings3XPerTerrain + terrainConfig.landings5XPerTerrain > terrainConfig.terrainPoints / 4)
+        {
+            throw new System.Exception("Terrain Generation: Too many landing spots for the ammount of points in map");
+        }
+        int aux = 0;
+        while (aux < terrainConfig.landings2XPerTerrain)
+        {
+            int tempPosX = Random.Range(2, terrainConfig.terrainPoints - 1);
+            if(!usedIndex.Contains(tempPosX - 1) && !usedIndex.Contains(tempPosX) && !usedIndex.Contains(tempPosX + 1))
+            {
+                usedIndex.Add(tempPosX - 1);
+                usedIndex.Add(tempPosX);
+                usedIndex.Add(tempPosX + 1);
+                Vector3 pos1 = terrainSpline.GetPosition(tempPosX - 1);
+                Vector3 pos2 = terrainSpline.GetPosition(tempPosX);
+                Vector3 pos3 = terrainSpline.GetPosition(tempPosX + 1);
+                terrainSpline.SetPosition(tempPosX - 1, new Vector3(pos1.x, pos2.y, 0));
+                terrainSpline.SetPosition(tempPosX + 1, new Vector3(pos3.x, pos2.y, 0));
+                Vector3 newLandingSitePos = new Vector3(pos2.x, pos2.y + terrainConfig.offsetFromSurface, 0);
+                GameObject go = Instantiate(terrainConfig.landingSitePrefab, newLandingSitePos, Quaternion.identity, transform);
+                go.GetComponent<LandingSite>().SetLandingType(terrainConfig.landingPoints2X, terrainConfig.landingSize2X, terrainConfig.landing2XUIColor);
+                aux++;
+            }
+        }
+        aux = 0;
+        while (aux < terrainConfig.landings3XPerTerrain)
+        {
+            int tempPosX = Random.Range(2, terrainConfig.terrainPoints);
+            if (!usedIndex.Contains(tempPosX - 1) && !usedIndex.Contains(tempPosX) && !usedIndex.Contains(tempPosX + 1))
+            {
+                usedIndex.Add(tempPosX - 1);
+                usedIndex.Add(tempPosX);
+                usedIndex.Add(tempPosX + 1);
+                Vector3 pos1 = terrainSpline.GetPosition(tempPosX - 1);
+                Vector3 pos2 = terrainSpline.GetPosition(tempPosX);
+                terrainSpline.SetPosition(tempPosX - 1, new Vector3(pos1.x, pos2.y, 0));
+                Vector3 newLandingSitePos = new Vector3(pos2.x, pos2.y + terrainConfig.offsetFromSurface, 0);
+                GameObject go = Instantiate(terrainConfig.landingSitePrefab, newLandingSitePos, Quaternion.identity, transform);
+                go.GetComponent<LandingSite>().SetLandingType(terrainConfig.landingPoints3X, terrainConfig.landingSize3X, terrainConfig.landing3XUIColor);
+                aux++;
+            }
+        }
+        aux = 0;
+        while (aux < terrainConfig.landings5XPerTerrain)
+        {
+            int tempPosX = Random.Range(2, terrainConfig.terrainPoints - 1);
+            if (!usedIndex.Contains(tempPosX - 1) && !usedIndex.Contains(tempPosX) && !usedIndex.Contains(tempPosX + 1))
+            {
+                usedIndex.Add(tempPosX - 1);
+                usedIndex.Add(tempPosX);
+                usedIndex.Add(tempPosX + 1);
+                Vector3 pos2 = terrainSpline.GetPosition(tempPosX);
+                Vector3 newLandingSitePos = new Vector3(pos2.x, pos2.y + terrainConfig.offsetFromSurface, 0);
+                GameObject go = Instantiate(terrainConfig.landingSitePrefab, newLandingSitePos, Quaternion.identity, transform);
+                go.GetComponent<LandingSite>().SetLandingType(terrainConfig.landingPoints5X, terrainConfig.landingSize5X, terrainConfig.landing5XUIColor);
+                aux++;
+            }
+        }
+    }
+
+}
---
+++ Sgro Adrian - Lunar Lander - Parcial 2/Assets/Scripts/ScriptableObjects/TerrainConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+[CreateAssetMenu(fileName = "Terrain Configuration", menuName = "Terrain/Configuration")]
+public class TerrainConfiguration : ScriptableObject
+{
+
+    [Header("Terrain Config")]
+    public GameObject terrainBase = null;
+    public TerrainLinesTypes currentTerrainLinesTypes = TerrainLinesTypes.Both;
+    public int terrainSize = 100;
+    public int terrainPoints = 150;
+    public float nextPointRange = 2f;
+    public float perlinMultiplier = 10f;
+    [Header("Landing Configuration")]
+    public GameObject landingSitePrefab = null;
+    public float offsetFromSurface = -3.5f;
+    public int landings2XPerTerrain = 7;
+    public int landingPoints2X = 2;
+    public float landingSize2X = .06f;
+    public Color landing2XUIColor = Color.green;
+    public int landings3XPerTerrain = 7;
+    public int landingPoints3X = 3;
+    public Color landing3XUIColor = Color.yellow;
+    public float landingSize3X = .04f;
+    public int landings5XPerTerrain = 7;
+    public int landingPoints5X = 5;
+    public Color landing5XUIColor = Color.red;
+    public float landingSize5X = .02f;
+}
