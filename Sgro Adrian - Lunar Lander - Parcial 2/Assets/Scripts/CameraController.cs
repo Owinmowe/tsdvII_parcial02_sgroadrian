dea32662bda6a9447c943674fc354ed549fed049
Adrian <Owinmowe@gmail.com>
2021-06-29 00:16:19 -0300

    Added camera follow Y position on high altitude.

--- Sgro Adrian - Lunar Lander - Parcial 2/Assets/Scripts/CameraController.cs
+++ Sgro Adrian - Lunar Lander - Parcial 2/Assets/Scripts/CameraController.cs
@@ -8,24 +8,34 @@ public class CameraController : MonoBehaviour
     [SerializeField] Ship player = null;
     [SerializeField] float playerClosePosition = 7f;
     [SerializeField] float playerBackUpPosition = 7f;
+    [SerializeField] float playerFollowYPosition = 10f;
     [SerializeField] float cameraZoomInDistance = 6f;
     [SerializeField] float cameraZoomInAltitude = 3f;
     [SerializeField] float cameraZoomOutDistance = 10f;
 
     Camera camComponent;
     bool closeUp = false;
-
+    float distanceFromPlayer = 0;
 
     private void Awake()
     {
         camComponent = GetComponent<Camera>();
+        distanceFromPlayer = Vector2.Distance(new Vector2(transform.position.x, transform.position.y), player.transform.position);
     }
 
     private void Update()
     {
         if (!closeUp && player.transform.position.y < playerClosePosition) CloseUp();
         else if(closeUp && player.transform.position.y > playerBackUpPosition) BackUp();
-        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+
+        if (player.transform.position.y > playerFollowYPosition)
+        {
+            FollowPlayerXY();
+        }
+        else
+        {
+            FollowPlayerX();
+        }
     }
 
     void CloseUp()
@@ -41,4 +51,15 @@ public class CameraController : MonoBehaviour
         camComponent.orthographicSize = cameraZoomOutDistance;
         transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -1f);
     }
+
+    void FollowPlayerXY()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y - distanceFromPlayer, transform.position.z);
+    }
+
+    void FollowPlayerX()
+    {
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+    }
+
 }
